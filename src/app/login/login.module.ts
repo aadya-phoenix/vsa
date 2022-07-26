ad93691b85207f73e76121d7bd1377fad48eff01
Jig's <er.jigneshkalasariya@gmail.com>
2022-07-26 11:17:31 +0530

    Login Component Updated

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { SharedModule } from '../shared/shared.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
     LoginRoutingModule
   ]
 })
