de1b3a0860fb9801eca9ebcbec047a767823ff88
aadya-phoenix <70717291+aadya-phoenix@users.noreply.github.com>
2022-07-26 11:19:56 +0530

    new constants push

--- src/app/shared/constants/dataConstants.ts
+++ src/app/shared/constants/dataConstants.ts
@@ -1,5 +1,10 @@
-export const dataConstant = {
-    users:{
-        
-    }
+export const dataConstants = {
+    Users:[
+        {id:1,roleId:1,username:'admin',password:'123'},
+        {id:2,roleId:2,username:'planner',password:'123'},
+        {id:3,roleId:3,username:'auditor',password:'123'},
+        {id:4,roleId:4,username:'vendor',password:'123'}
+    ]
+       
+    
 }
