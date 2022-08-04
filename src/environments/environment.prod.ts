9861e95b564cc3eb0e5276f6b1a4f8e64308aa07
aadya-phoenix <70717291+aadya-phoenix@users.noreply.github.com>
2022-08-04 13:05:21 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:'https://orange.mindscroll.info/',
+  apiVersion:'v4',
+  clientId:'1',
+  clientSecret:'daBIcLqqwIOYrtdSJ6xT5i6omb75LSB5XCRhG9eG',
+    lang:'english'
+
 };
